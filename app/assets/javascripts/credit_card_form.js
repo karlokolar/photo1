09ed8c1ec1b6a0d222efd734a87308e0027cbe72
Karlo Kolar <alien@kolar.link>
2017-05-18 19:47:47 +0200

    Stripe working

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -1,50 +1,55 @@
-$(document).ready(function() {
-  var show_error, stripeResponseHandler, submitHandler;
+$(document).ready(function(){
+	var show_error, stripeResponseHandler, submitHandler;
 
-  submitHandler = function (event) {
-    var $form = $(event.target);
-    $form.find("input[type=submit]").prop("disabled", true);
-//If Stripe was initialized correctly this will create a token using the credit card info
-  if(Stripe){
-    Stripe.card.createToken($form, stripeResponseHandler);
-  } else {
-    show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
-  }
-  return false;
-};
+	submitHandler = function (event) {
+		var $form = $(event.target);
+		$form.find("input[type=submit]").prop("disabled", true);
+		//If Stripe was initialized correctly this will create a token using the credit card info
+		if(Stripe){
+			Stripe.card.createToken($form, stripeResponseHandler);
+			show_error("CC token generated")
+		} else {
+			show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
+		}
 
-$(".cc_form").on('submit', submitHandler);
+		return false;
 
-stripeResponseHandler = function (status, response) {
-  var token, $form;
+		};
+	// calling the SUBMIT
+	$(".cc_form").on('submit', submitHandler);
 
-  $form = $('.cc_form');
-  if (response.error) {
-    console.log(response.error.message);
-    show_error(response.error.message);
-    $form.find("input[type=submit]").prop("disabled", false);
-  } else {
-    token = response.id;
-    $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
+	// RESPONSE HANDLER
+	stripeResponseHandler = function (status, response) {
+			var token, $form;
+			$form = $('.cc_form');
+			if (response.error) {
+				console.log(response.error.message);
+				show_error(response.error.message);
+				$form.find("input[type=submit]").prop("disabled", false);
+			} else {
+				token = response.id;
+				$form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
+				$("[data-stripe=number]").remove();
+				$("[data-stripe=cvv]").remove();
+				$("[data-stripe=exp-year]").remove();
+				$("[data-stripe=exp-month]").remove();
+				$("[data-stripe=label]").remove();
+				$form.get(0).submit();
+			}
 
-    $("[data-stripe=number]").remove();
-    $("[data-stripe=cvv]").remove();
-    $("[data-stripe=exp-year]").remove();
-    $("[data-stripe=exp-month]").remove();
-    $("[data-stripe=label]").remove();
+			return false;
 
-    $form.get(0).submit();
-  }
-  return false;
-};
+	};
 
-show_error = function (message) {
-  if($("#flash-messages").size() < 1){
-    $('div.container.main div:first').prepend("<div id='flash-messages'></div>")
-  }
+	//ERROR HANDLING
+	show_error = function (message) {
+		if($("#flash-messages").size() < 1){
+			$('div.container.main div:first').prepend("<div id='flash-messages'></div>")
+			}
+			$("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
+			$('.alert').delay(5000).fadeOut(3000);
 
-  $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
-  $('.alert').delay(5000).fadeOut(3000);
-  return false;
-  };
+		return false;
+	};
+
 });
