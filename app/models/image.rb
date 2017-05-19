02c76ba6cb3065a9f492fefff935c5456f85d371
Karlo Kolar <alien@kolar.link>
2017-05-19 17:28:15 +0200

    integration of AWS

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,13 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "Size of the picture should be less then 5 Mb")
+    end
+  end
 end
